2f640eabacf0bb1869fbacb477b41aa43dd24b6d
n555585 <jorge.vazquezjimenez@gruposantander.es>
2021-12-14 15:13:25 +0100

    RKE cluster module

--- main.tf
+++ main.tf
@@ -15,6 +15,18 @@ provider "rancher2" {
   insecure = true
 }
 
+module "rke_cluster" {
+  source  = "app.terraform.io/sanesp-poc/rke_cluster/rancher2"
+  version = "0.0.4"
+  
+  api_url = "https://sanes-rancher.westeurope.cloudapp.azure.com"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  description = var.description
+  name = var.name
+  kubernetes_network_plugin = var.kubernetes_network_plugin
+}
+
 module "etcd" {
   source  = "app.terraform.io/sanesp-poc/node_pool/rancher2"
   version = "0.0.3"
